a9e5a8a4e307438c32d1d54e58b7c2ca86335e23
victor <beorn57@hotmail.com>
2018-10-17 17:42:25 +0200

    estructura php

---
+++ web/php/Controlador/iArticulo.php
@@ -0,0 +1,13 @@
+<?php 
+	interface iArticulo
+	{
+		public function selectALL(); 							//Saca todos los articulos publicados ordenados por fecha
+		public function selectBY($nombre, $email, $fecha);		/*Saca los articulos filtrados por nombre del autor 
+																 (o su email) y/o la fecha de publicación*/
+		public function delete($idArticulo,$whoOrder);			/*Borra un articulo identificando si es el autor o el 
+																  administrador*/
+		public function update($idArticulo, $fecha, $texto, $idOrder); /*Actualiza el contenido del articulo (solo el autor o
+	                                                                     el administrador)*/
+	}
+
+?>
