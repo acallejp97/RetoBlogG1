930a1c89d7be358fc986da9d7da3e0ba0fa33696
acallejp97 <acallejp@gmail.com>
2018-10-22 11:52:10 +0200

    22/10

--- web/js/validacionFormulario.js
+++ web/js/validacionFormulario.js
@@ -1,5 +1,6 @@
 //Funcion que detecta si se ha escrito algo dentro de un input
 function validacionTexto(texto) {
+  texto = texto.element[""];
   if (texto.value == "") {
     alert("El " + texto.name + " no puede estar vacio");
     return false;
@@ -9,7 +10,7 @@ function validacionTexto(texto) {
 //Validamos si la fecha introducida es anterior al dia de hoy
 function validarFechaProgramada(fecha) {
   var fechaHoy = new Date();
-  if (fechaHoy <= fecha) {
+  if (fechaHoy <= fecha.element[""]) {
     alert("La fecha introducida no es valida");
     return false;
   } else return true;
