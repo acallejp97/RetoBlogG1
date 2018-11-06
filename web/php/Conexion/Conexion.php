0baf8acda3c6b143ad61e4696a557b50785448ed
acallejp97 <acallejp@gmail.com>
2018-11-06 11:53:32 +0100

    arreglo conexion

--- web/php/Conexion/Conexion.php
+++ web/php/Conexion/Conexion.php
@@ -2,61 +2,60 @@
 
 class Conexion
 {
-    		private $db="G1Reto";
-		private $servidor="mysql:host=127.0.0.1;dbname=";
-		private $username="root";
-		private $password="";
-	        private $conn;
-		private static $instance=NULL;
-		
-		private function __construct()
-                {
-                   
-                }
- 
-		private function __clone(){}
-		
-		public static function getInstance(){
-		 if (!isset(self::$instance)) 
-                   {
-		    $pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-                    self::$instance= new PDO("mysql:host=127.0.0.1;dbname="."g1reto","root","",$pdo_options);
-		    }
-			return self::$instance;
-                }
-                
-                public static function cerrar()
-                {
-                    $this->instance=null;
-                }
-                /*function __construct() 
-                {
-                     $this->db = "g1blog";
-                     $this->username = "root";
-                     $this->password = "";
-                }
+    private $db = "G1Reto";
+    private $servidor = "mysql:host=127.0.0.1;dbname=";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+    private static $instance = null;
 
-                //Creamos conexión
-		public function conectar()
-		{
-                    try
-				{
-                                        
-                                      	$this->conn=new PDO($this->servidor, $this->username, $this->password);
-					$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					echo "<br><p>Conexión realizada con exito</p><br>";
-				}
-				catch (PDOException $e)
-				{
-					echo "Conexión fallida: ".$e->getMessage();
-				}
-				return $this->conn;
-			}
+    private function __construct()
+    {
 
-			public function cerrarConn()
-			{
-				$this->conn=null;
-			}*/
-	}
-?>
+    }
 
+    private function __clone()
+    {}
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+            self::$instance = new PDO("mysql:host=127.0.0.1;dbname=" . "g1reto", "root", "", $pdo_options);
+        }
+        return self::$instance;
+    }
+
+    public static function cerrar()
+    {
+        $instance = null;
+    }
+    /*function __construct()
+{
+$this->db = "g1blog";
+$this->username = "root";
+$this->password = "";
+}
+
+//Creamos conexión
+public function conectar()
+{
+try
+{
+
+$this->conn=new PDO($this->servidor, $this->username, $this->password);
+$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "<br><p>Conexión realizada con exito</p><br>";
+}
+catch (PDOException $e)
+{
+echo "Conexión fallida: ".$e->getMessage();
+}
+return $this->conn;
+}
+
+public function cerrarConn()
+{
+$this->conn=null;
+}*/
+}
