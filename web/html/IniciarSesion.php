34986351eb842e73975076c82b074cd64929ed6a
victor <beorn57@hotmail.com>
2018-11-08 20:42:47 +0100

    cambio_carpeta_iniciar_sesion

--- web/html/IniciarSesion.php
+++ web/html/IniciarSesion.php
@@ -1,16 +1,15 @@
 <?php
 session_start();
+
 define('RAIZ_APLICACION', dirname(__FILE__));
 
-include_once RAIZ_APLICACION . '/../php/Controlador/usuarioDTO.php';
-include_once RAIZ_APLICACION . '/../php/Controlador/UsuarioControler.php';
+include_once RAIZ_APLICACION . '../Controlador/usuarioDTO.php';
+include_once RAIZ_APLICACION . '../Controlador/UsuarioControler.php';
+
 $usuario = $_POST["usuario"];
 $pwd = $_POST["password"];
 $usuarioDto = new usuarioDTO();
 $usuarioControl = new UsuarioController();
 $usuarioDto = $usuarioControl->login($usuario, $pwd);
-$usuario = ["idUsuario" => $usuarioDto->getIdUsuario(),
-    "nombre" => $usuarioDto->getNombre()];
 $_SESSION["usuario"] = $usuarioDto;
-echo $usuarioDto->toString();
-//header('Location: index1.php');
+header("Location: " . RAIZ_APLICACION . "/index1.php");
