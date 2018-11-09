18b3bcb18123d31d79de24e430bf50df6640d855
ik_2dw3 <jon.callejaga@elorrieta-errekamari.com>
2018-11-09 10:12:28 +0100

    gdaskg

--- web/html/index1.php
+++ web/html/index1.php
@@ -31,7 +31,7 @@ if (isset($_SESSION)) {
 
 <body>
     <article>
-        <aside>
+        <aside id="aside" onload="ocultarAside();">
             <h1>Criterio de busqueda</h1>
             <p>
                 <form action="BuscarCustom.php" method="POST">
@@ -90,7 +90,12 @@ echo $vista->mostrarContenido();
 				return console.log(false);
 			}
 		}
+
+		function ocultarAside(){
+			document.getElementById("aside").style.visibility="hidden";
+		}
 	</script>
+
 	<footer>
 		<a href="about.html">About</a>
 	</footer>
